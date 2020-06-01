a13c064a505a405f0b2f88266af909af3720dd2a
MD-Nasir301 <nasir21b@gmail.com>
2020-06-02 00:18:00 +0600

    test git login

--- src/Component/ListInputForm/ListInputForm.js
+++ src/Component/ListInputForm/ListInputForm.js
@@ -3,16 +3,13 @@ import './ListInputForm.css'
 import { useForm } from 'react-hook-form'
 import { useState } from 'react';
 const ListInputForm = () => {
-    //  const getData =  JSON.parse(localStorage.getItem('listItem'))
 
+     const getData =  JSON.parse(localStorage.getItem('data'))
 
-    const { register, handleSubmit, } = useForm()
+    const { register, handleSubmit, } = useForm()   
 
     const [inputField, setInputField] = useState([{"name":1},{"name":2},{"name":3},{"name":4},{"name":5}])
-    const [localArray, setLocalArray] = useState()
     const [count,setCount] = useState(0)
-    const [listData,setListData] = useState()
-
 
     const data = [
         {
@@ -39,18 +36,20 @@ const ListInputForm = () => {
         },
     ]
 
+    const [dat ,setDat] = useState([])
 
     const onSubmit = data => {
-         setListData(data)
-         setCount(count+1)
-         setLocalArray([...localArray,data])
-         console.log("bbbbbbbbbbbbbb",localArray.length);
-         localStorage.setItem("listItem",JSON.stringify(localArray))
+        
+           setDat([...dat,data]) 
+           localStorage.setItem("data",JSON.stringify(dat))
+
+           document.getElementById("myForm").reset();
+           
         }
 
     const addInputField = ()=> {
         setCount(count+1) 
-        setInputField([...inputField,{"name":"input"+count}])
+        setInputField([...inputField,{"name": count}])
     }
 
 
@@ -58,10 +57,10 @@ const ListInputForm = () => {
         <div>
             <div className="list-input-form-area">
                 
-                <form onSubmit={handleSubmit(onSubmit)}>
+                <form id="myForm" onSubmit={handleSubmit(onSubmit)}>
                 <div className="inputFields">
                 {
-                    inputField.map(name =>  <input name={name.name} ref={register} />)
+                    inputField.map(name =>  <input id="a" name={name.name} ref={register} />)
                 }
                 </div>
    
@@ -69,18 +68,20 @@ const ListInputForm = () => {
                  <button type="submit">Submit</button>
                 </div>
                 </form>
+
               <div className="add-input-field">
                  <button onClick={addInputField}>add</button>
             </div>
+
             </div>
 
             <div id="list-box-area">
                 {
-                    data.map(item => <div className="list-box">  
+                getData ?  getData.map(item => <div className="list-box">  
                             {
                             Object.keys(item).map((a,b) => <li> { item[a]} </li> )
                             } 
-                        </div>)
+                        </div>) : <div></div>
                 }
             </div>
 
@@ -88,5 +89,6 @@ const ListInputForm = () => {
         </div>
     );
 };
+// abcd
 
 export default ListInputForm;
